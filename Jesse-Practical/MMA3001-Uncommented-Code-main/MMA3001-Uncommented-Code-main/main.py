2140797fa98fad75e1e8b4d0db9dec2548fd6da2
Jesse-Goldie <jgol0011@student.monash.edu>
2026-07-29 15:14:32 +1000

    Created using Colab

--- Jesse-Practical/MMA3001-Uncommented-Code-main/MMA3001-Uncommented-Code-main/main.py
+++
@@ -1,7 +0,0 @@
-from generator import generate_noisy_images
-from averager import average_images
-
-if __name__ == "__main__":
-    output_dir = "generated_images"
-    generate_noisy_images(output_dir, n_images=30)
-    average_images(output_dir, output_path="denoised.png")
