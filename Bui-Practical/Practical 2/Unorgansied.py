146a1dc4b2e36c5fbcb1099fb84de41d2bba43cc
Chris Bui <cbui0013@student.monash.edu>
2026-08-05 14:21:53 +1000

    Added .venv file and started Q1 for Prac W2

---
+++ Bui-Practical/Practical 2/Unorgansied.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+"""1. Variables, Their Binary Representation, Bits, Bytes and Memory Requirements
+"""
+
+print(f"There are {np.int8().itemsize} bytes in int8")
+print(f"There are {np.int32().itemsize} bytes in int32")
+print(f"There are {np.uint32().itemsize} bytes in uint32")
+print(f"There are {np.int64().itemsize} bytes in int64")
+print(f"There are {np.float32().itemsize} bytes in float32")
+print(f"There are {np.float64().itemsize} bytes in float64")
